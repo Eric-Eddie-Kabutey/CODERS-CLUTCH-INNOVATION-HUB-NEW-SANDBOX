5fab41c5ed986715a0b206271c89d7ecc6e29436
coded <coded@gmail.com>
2025-09-24 18:49:52 +0000

    build error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 
 import Hero from "@/components/new-landing-page/Hero";
-import NewNavbar from "@/components/new-landing-page/navbar";
+
 
 export default function Home() {
   return (
