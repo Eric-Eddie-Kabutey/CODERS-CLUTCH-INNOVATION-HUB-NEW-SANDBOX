c18613f8c33880f489ff51acf07615b8eef7396e
coded <coded@gmail.com>
2025-09-26 14:54:29 +0000

    build error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import WhoWeAre from "@/components/home/WhoWeAre";
 import CaseStudies from "@/components/new-landing-page/CaseStudies";
 import Challenges from "@/components/new-landing-page/challenges";
 import ContactSection from "@/components/new-landing-page/ContactSection";
-import Covered from "@/components/new-landing-page/Covered";
 import Events from "@/components/new-landing-page/Event";
 import FoundersCompass from "@/components/new-landing-page/FoundersCompass";
 import Hero from "@/components/new-landing-page/Hero";
@@ -13,7 +12,6 @@ import NetworkSection from "@/components/new-landing-page/NetworkSection";
 import NewFooter from "@/components/new-landing-page/NewFooter";
 import Organization from "@/components/new-landing-page/organization";
 import Testimonials from "@/components/new-landing-page/Testimonials";
-import WhatClientSays from "@/components/new-landing-page/what-client-says";
 
 
 export default function Home() {
@@ -25,7 +23,6 @@ export default function Home() {
       <CaseStudies />
       <NetworkSection />
       <Events />
-      
       <Organization />
       <FoundersCompass />
       <Innovation />
